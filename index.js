66d3e7b24196ad216e56c150600dbea79e06bf2e
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2023-07-15 18:34:17 +1000

    export prompt

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Assistant from "./ai-assistant.js";
 import Prompt from "./prompt.js";
 
-export default Assistant;
+export { Assistant };
+export { Prompt };
 
 const assistant = new Assistant("apiKey");
 const newPrompt = new Prompt("I really want to go out to the cinemas sometime", ["funnier"], ["be a better surfer"]);
