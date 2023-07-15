324bf2bc62682f4c7520408c9e1924cda76ebedf
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2023-07-15 18:29:36 +1000

    fix

--- prompt.js
+++ prompt.js
@@ -7,33 +7,27 @@ export default class Prompt{
      * @param {string[]} senderIntendedPersona 
      * @param {string[]} recieverIntendedPersona
      * @param {string} previousMsg 
-     * @param {string} mode
      */
-    constructor(userMsg, senderIntendedPersona, recieverIntendedPersona, mode, previousMsg=null){
+    constructor(userMsg, senderIntendedPersona, recieverIntendedPersona, previousMsg=null){
         this.userMsg = userMsg
         this.senderIntendedPersona = senderIntendedPersona
         this.recieverIntendedPersona = recieverIntendedPersona
         this.previousMsg = previousMsg
-        this.mode = mode
     }
 
     toString(){
-        if(mode == "casual"){
-            return `
-            There is person 1 who wants these intended personas- ${this.senderIntendedPersona.join(", ")}, 
-            there is also person 2 who has these intended personas- ${this.recieverIntendedPersona.join(", ")}, 
-            alter this message sent by person 1 to person 2 ${this.userMsg} 
-            ${this.previousMsg == null ? "" : `so that person 1 replies to person 2's message of ${this.previousMsg}`} 
-            that it alligns with person 1's intended personas 
-            and it also appeals to person 2 with their personas.
-            BE VERY SUBTLE WITH THE ALTERATIONS!
-            ONLY RESPOND WITH THE MESSGE ITSELF, NOTHING ELSE!
-            NEVER REFERENCE EACH PERSON BY NAME!
-            `
-        }
-        else{
-            throw Error("Unsupported Mode")
-        }
+        return `
+        There is person 1 who wants these intended personas- ${this.senderIntendedPersona.join(", ")}, 
+        there is also person 2 who has these intended personas- ${this.recieverIntendedPersona.join(", ")}, 
+        alter this message sent by person 1 to person 2 ${this.userMsg} 
+        ${this.previousMsg == null ? "" : `so that person 1 replies to person 2's message of ${this.previousMsg}`} 
+        that it alligns with person 1's intended personas 
+        and it also appeals to person 2 with their personas.
+        BE VERY SUBTLE WITH THE ALTERATIONS!
+        ONLY RESPOND WITH THE MESSGE ITSELF, NOTHING ELSE!
+        NEVER REFERENCE EACH PERSON BY NAME!
+        `
+        
     }
 
     /**
